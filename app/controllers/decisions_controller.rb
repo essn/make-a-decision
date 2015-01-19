f7cbe8f0902816f21ff9f220a83230fda1f2edcf
Alex Stophel <alexstophel@gmail.com>
2015-01-19 18:35:05 -0500

    Finished form form for new decision and updated decisions_controller.

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -1,11 +1,30 @@
 class DecisionsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def new
+    @decision = Decision.new
   end
 
   def create
-    @decision
+    @decision = current_user.decisions.build(decision_params)
+    if @decision.save
+      redirect_to @decision
+    end
   end 
 
   def index
+
+  end
+
+  def show
+    @decision = Decision.find(params[:id])
+  end
+
+  private
+
+  def decision_params
+    params.require(:decision).permit(:dilemma, :needed_by, :decision_importance, :indecisiveness_level, :best_case,
+                                     :worst_case, :gut_feeling, :pluses, :minuses, :intuitive_conclusion,
+                                     :rational_conclusion, :next_steps)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150119215443) do
+ActiveRecord::Schema.define(version: 20150119225309) do
 
   create_table "decisions", force: true do |t|
     t.string   "decision"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150119215443) do
     t.string   "next_steps"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
