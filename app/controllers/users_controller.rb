490583c851e1c3f5f91504cc24615eb41f3e364e
Jesse Pledger <JessePledger@gmail.com>
2015-01-23 20:18:46 -0800

    Fixed issue displaying users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :find_user, only: [:show]
 
   def show
-    @decisions = @user.decisions.all
+    @decisions = Decision.find(params[:id])
   end
 
   private
