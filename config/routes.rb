4ba3a93d2dba39d9eb1c2578e365683d01ddae37
Jesse Pledger <JessePledger@gmail.com>
2015-01-28 19:08:05 -0800

    Added /decisions to show decisions#index to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     resources :decisions
   end
 
+  get '/decisions', to: 'decisions#index', as: 'decisions'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
